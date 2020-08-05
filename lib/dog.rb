b2c715dcdc63b9cf1cf5f93096f4c31083d538c4
Sean B <butts.sean01@gmail.com>
2020-08-05 09:17:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,12 @@
 # Your code goes here!
 class Dog
   def name=(dog_name)
-    
+    this_dogs_name = dog_name
   end
+    
+    def name
+      dog_name
+    end
     
   def bark
     puts "woof!"
